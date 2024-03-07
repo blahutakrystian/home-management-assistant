1d3348494641d100afdcb4bb14a342da94176fdf
blahutakrystian <blahutakrystian@gmail.com>
2024-03-07 23:10:50 +0100

    fix(mainactivity): adjust to mvvm

--- HMA.Mobile/app/src/main/java/com/example/hma_mobile/ui/screens/activity/MainActivity.kt
+++ HMA.Mobile/app/src/main/java/com/example/hma_mobile/ui/screens/activity/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.hma_mobile.ui.screens.activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -11,19 +13,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.hma_mobile.ui.theme.HmamobileTheme
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    private val splashViewModel: MainActivityViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply{
+            setKeepOnScreenCondition { splashViewModel.isLoading.value }
+        }
         setContent {
             HmamobileTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-
-                }
+                //MainScreen()
             }
         }
     }
@@ -31,10 +32,10 @@ class MainActivity : ComponentActivity() {
 
 
 
-@Preview(showBackground = false)
-@Composable
-fun GreetingPreview() {
-    HmamobileTheme {
-
-    }
-}
+//@Preview(showBackground = false)
+//@Composable
+//fun GreetingPreview() {
+//    HmamobileTheme {
+//
+//    }
+//}
