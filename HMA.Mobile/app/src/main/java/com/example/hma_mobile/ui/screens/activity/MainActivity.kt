70282f6ae98d97b2eddb4f9508eef3f8dc5338c9
blahutakrystian <blahutakrystian@gmail.com>
2024-03-10 20:05:17 +0100

    feat(mainscreen): add MainScreen

--- HMA.Mobile/app/src/main/java/com/example/hma_mobile/ui/screens/activity/MainActivity.kt
+++ HMA.Mobile/app/src/main/java/com/example/hma_mobile/ui/screens/activity/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         }
         setContent {
             HmamobileTheme {
-                //MainScreen()
+                MainScreen()
             }
         }
     }
---
+++ HMA.Mobile/app/src/main/java/com/example/hma_mobile/ui/screens/mainscreen/MainScreen.kt
@@ -0,0 +1,8 @@
+package com.example.hma_mobile.ui.screens.mainscreen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainScreen(){
+    
+}
