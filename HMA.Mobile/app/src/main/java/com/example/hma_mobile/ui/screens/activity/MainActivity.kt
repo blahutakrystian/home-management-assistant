f809bbcfb9991ab00cfdf446df89afecfeccf4cb
blahutakrystian <blahutakrystian@gmail.com>
2024-03-10 20:06:44 +0100

    fix(mainactivity): fix MainScreen reference

--- HMA.Mobile/app/src/main/java/com/example/hma_mobile/ui/screens/activity/MainActivity.kt
+++ HMA.Mobile/app/src/main/java/com/example/hma_mobile/ui/screens/activity/MainActivity.kt
@@ -1,19 +1,12 @@
 package com.example.hma_mobile.ui.screens.activity
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.hma_mobile.ui.theme.HmamobileTheme
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.hma_mobile.ui.screens.mainscreen.MainScreen
+import com.example.hma_mobile.ui.theme.HmamobileTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
