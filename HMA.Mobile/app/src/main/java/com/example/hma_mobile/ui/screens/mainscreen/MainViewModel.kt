35c822df2c213b65463382bc4b366818e3a98054
blahutakrystian <blahutakrystian@gmail.com>
2024-03-11 21:15:13 +0100

    feat(mainscreen): add MainViewModel

---
+++ HMA.Mobile/app/src/main/java/com/example/hma_mobile/ui/screens/mainscreen/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.hma_mobile.ui.screens.mainscreen
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class MainViewModel @Inject constructor() : ViewModel() {
+}
