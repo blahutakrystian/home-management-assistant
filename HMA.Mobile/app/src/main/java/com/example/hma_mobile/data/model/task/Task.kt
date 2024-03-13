d3f52005d04bfc98517e7c8771b73c1753a2fe29
blahutakrystian <blahutakrystian@gmail.com>
2024-03-13 22:08:59 +0100

    feat(task): add task data class

---
+++ HMA.Mobile/app/src/main/java/com/example/hma_mobile/data/model/task/Task.kt
@@ -0,0 +1,5 @@
+package com.example.hma_mobile.data.model.task
+
+data class Task (
+    val id: Int
+)
--- HMA.Mobile/app/src/main/java/com/example/hma_mobile/data/repository/TaskRepositoryInterface.kt
+++ HMA.Mobile/app/src/main/java/com/example/hma_mobile/data/repository/TaskRepositoryInterface.kt
@@ -1,5 +1,6 @@
 package com.example.hma_mobile.data.repository
 
+import com.example.hma_mobile.data.model.task.Task
 import kotlinx.coroutines.flow.Flow
 
 
