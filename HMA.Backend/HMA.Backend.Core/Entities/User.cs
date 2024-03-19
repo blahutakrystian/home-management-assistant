75c1201f3b563a90007baf06cd358efee31e6d4f
blahutakrystian <blahutakrystian@gmail.com>
2024-03-19 22:25:42 +0100

    feat(user): add user entity

---
+++ HMA.Backend/HMA.Backend.Core/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMA.Backend.Core.Entities
+{
+    public class User
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+
+        public User(Guid id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+    }
+}
