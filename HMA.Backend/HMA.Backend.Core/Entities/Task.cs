06e2e60ca88bcbabb47703143517fc2811386fe5
blahutakrystian <blahutakrystian@gmail.com>
2024-03-20 20:10:44 +0100

    feat(task entity): add task entity

---
+++ HMA.Backend/HMA.Backend.Core/Entities/Task.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMA.Backend.Core.Entities
+{
+    public class Task
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public bool IsDone { get; private set; }
+
+        public Task(Guid id, string name, string description, bool isDone)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            IsDone = isDone;
+        }
+    }
+}
