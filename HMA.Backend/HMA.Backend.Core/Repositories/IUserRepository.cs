694fa7e7ac567971f2c74c6802c42dffc608e148
blahutakrystian <blahutakrystian@gmail.com>
2024-03-21 20:45:41 +0100

    feat(iuserrepository): add user repository contract

---
+++ HMA.Backend/HMA.Backend.Core/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using HMA.Backend.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMA.Backend.Core.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUserByIdAsync(Guid id);
+    }
+}
