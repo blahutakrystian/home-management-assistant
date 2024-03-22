32d72395c5393735d5a5ac2f6e07699ebaa22a18
blahutakrystian <blahutakrystian@gmail.com>
2024-03-22 22:56:16 +0100

    feat(itaskrepository): add task repository contract

---
+++ HMA.Backend/HMA.Backend.Core/Repositories/ITaskRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Task = HMA.Backend.Core.Entities.Task;
+
+namespace HMA.Backend.Core.Repositories
+{
+    public interface ITaskRepository
+    {
+        Task<Task> GetTaskByIdAsync(Guid id);
+    }
+}
